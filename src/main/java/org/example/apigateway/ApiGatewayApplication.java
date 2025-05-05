600cb9d5f9b2bcfab7ee3356abc03436bd4f0514
is6769 <igor.shvets2007@mail.ru>
2025-05-05 13:31:18 +0300

    added sso service and made first api gateway successful redirect

--- src/main/java/org/example/apigateway/ApiGatewayApplication.java
+++ src/main/java/org/example/apigateway/ApiGatewayApplication.java
@@ -1,8 +1,12 @@
 package org.example.apigateway;
 
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.filter.factory.GatewayFilterFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -10,6 +14,14 @@ public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+	@Bean
+	public ApplicationRunner filterDebug(ApplicationContext ctx) {
+		return args -> {
+			System.out.println("REGISTERED FILTERS: " +
+					ctx.getBeansOfType(GatewayFilterFactory.class).keySet());
+		};
 	}
 
 }
---
+++ src/main/java/org/example/apigateway/filters/JwtHeaderFilter.java
@@ -0,0 +1,74 @@
+package org.example.apigateway.filters;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.Map;
+
+@Component
+public class JwtHeaderFilter extends AbstractGatewayFilterFactory<JwtHeaderFilter.Config> {
+
+    private static final Logger log = LoggerFactory.getLogger(JwtHeaderFilter.class);
+    private final WebClient webClient;
+
+
+    public JwtHeaderFilter(WebClient.Builder webClientBuilder) {
+        super(Config.class);
+        this.webClient = webClientBuilder.build();
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+
+        return (exchange, chain) -> {
+            log.info("Entering JwtHeaderFilter for path: {}", exchange.getRequest().getPath());
+            String authHeader = exchange.getRequest().getHeaders().getFirst("Authorization");
+            if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+                exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+                return exchange.getResponse().setComplete();
+            }
+
+            String token = authHeader.replace("Bearer ", "");
+
+            log.info(token);
+
+            return webClient.post()
+                    .uri("http://localhost:8765/auth/introspection")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .bodyValue(Map.of("token", token))
+                    .retrieve()
+                    .bodyToMono(Map.class)
+                    .flatMap(response -> {
+                        log.info(response.toString());
+                        if (Boolean.TRUE.equals(response.get("active"))) {
+                            log.info("Active");
+                            var modifiedRequest = exchange.getRequest()
+                                    .mutate()
+                                    .header("X-User-Id", response.get("ref_id") != null ?
+                                            response.get("ref_id").toString() : "MANAGER")
+                                    .header("X-User-Role", response.get("role").toString())
+                                    .build();
+                            return chain.filter(exchange.mutate().request(modifiedRequest).build());
+                        }
+                        log.info("Inactive");
+                        exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+                        return exchange.getResponse().setComplete();
+                    })
+                    .onErrorResume(e -> {
+                        log.info(e.toString());
+                        exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+                        return exchange.getResponse().setComplete();
+                    });
+        };
+    }
+
+    public static class Config {
+        // Add configuration properties here if needed
+    }
+}
